96d6fe7472965e3e7b6276a2d36843d3953037e3
Ritchel M <ritchiemesidor84@gmail.com>
2024-09-26 21:00:22 -0400

    I created a user class, added a function to stagger text, and have something that capitalizes the first letter of the user's name.

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
-import time
+import time, os, random
+# from user import *
 
-print("Welcome, aspiring entrepreneur\n\n")
+#creates a function to clear the console of text
+clear = lambda: os.system('cls')
+
+#Creates the class that keeps track of all user attributes
+class User:
+    def __init__(self, name):
+        self.name = name
+    def urmom(self, mom):
+        self.mom = mom
+    def set_occupation(self):
+        pass
+    def set_current_job(self, job):
+        self.job = job
+
+def stagger_text(text):
+    for i in text:
+        print(i, end='', flush=True)
+        time.sleep(0.1)
+    print('')
+jobs = ["McDonalds cashier", "Publix stocker"]
+# jobs_degree = {}
+
+print("Welcome, aspiring entrepreneur\n")
 
 time.sleep(1)
-
+#asks user for name and changes the first letter to uppercase
 name = input("Please enter your name to begin: ")
+10
+name = name.replace(f"{name[0]}", f"{name[0].upper()}", 1)
+
+
+player = User(name)
+player.set_current_job(random.choice(jobs))
+
+
+
+time.sleep(1)
+clear()
---
+++ user.py
@@ -0,0 +1,5 @@
+class User:
+    def __init__(self):
+        pass
+    def urmom(self, mom):
+        self.mom = mom
