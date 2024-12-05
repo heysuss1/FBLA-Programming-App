d7a83cf210afb3dcfb72f6ddb93f13e03a9ef8a1
Ritchel M <ritchiemesidor84@gmail.com>
2024-12-05 09:47:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import time, os, random
-# from user import *
-
-#creates a function to clear the console of text
+import time, os, random  
 clear = lambda: os.system('cls')
 
-#Creates the class that keeps track of all user attributes
+
+#Creates a class to keep track of all user attributes
 class User:
     def __init__(self, name):
         self.name = name
@@ -15,27 +13,47 @@ class User:
     def set_current_job(self, job):
         self.job = job
 
-def stagger_text(text):
+"""
+Params: Text to be displayed by user
+Output
+"""
+def stagger_text(text: str):
     for i in text:
         print(i, end='', flush=True)
         time.sleep(0.1)
     print('')
-jobs = ["McDonalds cashier", "Publix stocker"]
+
+companies = ["McDonalds", "Publix", "IBM"]
+company_job = {"McDonalds": "cashier", "Publix" : "stocker", "IBM": "data entry clerk"}
 # jobs_degree = {}
 
-print("Welcome, aspiring entrepreneur\n")
+def show_menu():
+    pass
+
+print("Welcome, aspiring entrepreneur")
 
 time.sleep(1)
 #asks user for name and changes the first letter to uppercase
 name = input("Please enter your name to begin: ")
-10
+clear()
 name = name.replace(f"{name[0]}", f"{name[0].upper()}", 1)
 
 
 player = User(name)
-player.set_current_job(random.choice(jobs))
+player.set_current_job(random.choice(companies)) 
 
+stagger_text(f"Time: 9:00 AM \n\nLocation: {player.job}")
 
+time.sleep(2)
 
-time.sleep(1)
 clear()
+
+print(f"You are a {company_job[player.job]} at {player.job}")
+time.sleep(1.5)
+print("You have spent 5 long, unfulfilling years working at your job")
+time.sleep(1.5)
+print("As a result, you decide to quit and start your own business")
+time.sleep(2)
+
+clear(  )
+product_type = input("")
